7f6043b1426be30e386e3e5c0f214e83547f71e6
anand311203 <146500419+anand311203@users.noreply.github.com>
2024-03-06 20:33:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ const App = () => {
     const [searchTerm,setSearchTerm] =useState('');
     const searchMovies = async (title) =>
      {
-        const response =await fetch(`${API_URL}&s=${title}`)
+        const response =await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
         setMovies(data.Search);
-     }
+     };
 
     useEffect( ()=> {
         searchMovies('avengers');
